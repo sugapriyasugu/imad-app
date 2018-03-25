3f6244e2e923b4c45daa6ee3f49849dcc9dab32e
Sugapriya S <sugapriyam21@gmail.com>
2018-03-25 10:49:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,13 @@ console.log('Loaded!');
 // change the content of main-text div
 var ele=document.getElementById('main-text');
 ele.innerHTML="Hello world";
+//move the image
+var img=document.getElementById('madi');
+var marginLeft=0;
+function moveRight(){
+    marginLeft+=1;
+    img.style.marginLeft=marginLeft+"px";
+}
+img.onclick=function(){
+    var interval=setInterval(moveRight,50);
+};
