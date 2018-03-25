f825d8b2ccfdd8bde4a555b89219cf17bbd440a0
Sugapriya S <sugapriyam21@gmail.com>
2018-03-25 10:53:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ ele.innerHTML="Hello world";
 var img=document.getElementById('madi');
 var marginLeft=0;
 function moveRight(){
-    marginLeft+=10;
+    marginLeft+=1;
     img.style.marginLeft=marginLeft+"px";
 }
 img.onclick=function(){
