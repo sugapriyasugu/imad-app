093bb1f7a151ee86d588160c657c64e5adf995e3
Sugapriya S <sugapriyam21@gmail.com>
2018-04-27 08:31:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,12 +85,16 @@ app.get('/',function(req,res){
     res.sendFile(path.join(__dirname,'ui','index.html'));
 });
 
+var counter=0;
+app.get('/counter',function(req,res){
+    counter+=1;
+    res.send(counter.toString());
+});
+
 app.get('/:articleName', function (req, res) {
   var articleName=req.params.articleName;
   res.send(createTemplate(articles[articleName]));
 });
-
-
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
