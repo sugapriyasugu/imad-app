ff2d4f593f8c604a299fc618dd6dc7484a397dd3
Sugapriya S <sugapriyam21@gmail.com>
2018-04-27 10:01:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,3 +18,19 @@ button.onclick=function(){
     request.open('GET','http://sugapriyam21.imad.hasura-app.io/counter');
     request.send(null);
 };
+
+//submit name
+let nameInput=document.getElementById('name');
+let name=nameInput.value;
+let submit=document.getElementById('submit_btn');
+submit.onclick=function(){
+  //Make a request to the server and send the name
+  //Capture a list of names and render it as a list
+  let names=[name1,name2,name3];
+  let list="";
+  names.forEach(function(name){
+      list+="<li>"+name+"</li>";
+  });
+  let ul=document.getElementById('namelist');
+  ul.innerHTML=list;
+};
